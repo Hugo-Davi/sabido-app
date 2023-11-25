92edefec5b51703a9bbd79cc54870e486073c079
Hugo-Davi <111790130+Hugo-Davi@users.noreply.github.com>
2023-11-25 15:03:10 -0300

    Sabido APP sem vulnerabilidades

---
+++ src/app/(tabs)/profile.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native'
+
+export default function Profile(){
+    return (
+        <View>
+            <Text style={{fontSize: 44}}>
+                Profile
+            </Text>
+        </View>
+    )
+}
