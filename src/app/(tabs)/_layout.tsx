c9eaaa1ff3ae2437999aa44577910e0e986b765c
Hugo-Davi <111790130+Hugo-Davi@users.noreply.github.com>
2023-11-27 20:55:20 -0300

    Custom tab

--- src/app/(tabs)/_layout.tsx
+++ src/app/(tabs)/_layout.tsx
@@ -1,18 +1,53 @@
+
+import { View } from 'react-native';
 import { Tabs } from "expo-router";
+
+import { MaterialCommunityIcons } from "@expo/vector-icons"
+import { Ionicons } from '@expo/vector-icons';
 
 export default function TabRoutesLayout() {
     return(
-        <Tabs>
+        <Tabs
+            screenOptions={{
+                tabBarStyle: {
+                    position: 'absolute',
+                    bottom: 0,
+                    right: 0,
+                    left: 0,
+                    height: 60,
+                    elevation: 0,
+                }
+            }}>
             <Tabs.Screen
                 name= "index"
                 options= {{
-                    title: "Home"
+                    title: "Home",
+                    tabBarIcon: ({color, size}) => <MaterialCommunityIcons name="cards-outline"
+                    color={color} size={size * 1.75} />
+                    }
+                }
+            />
+            <Tabs.Screen
+                name="teste"
+                options={{
+                    title: 'teste',
+                    tabBarIcon: ({color, size}) => <Ionicons name="add-circle"
+                        color={color} size={size * 3}
+                        style={{
+                            position: 'relative',
+                            right: 0,
+                            left: 0,
+                            top: 0,
+                            height: 100
+                        }}
+                    />
                 }}
             />
             <Tabs.Screen
                 name="profile"
                 options= {{
-                    title: "Profile"
+                    title: "Profile",
+                    tabBarIcon: ({color, size}) => <Ionicons name="ios-person-circle" color={color} size={size * 1.75} />
                 }}
             />
         </Tabs>
