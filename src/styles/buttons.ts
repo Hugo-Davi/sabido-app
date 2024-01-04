fc7e7c58b98f500449892522da31a1e50e365660
Hugo-Davi <111790130+Hugo-Davi@users.noreply.github.com>
2024-01-03 23:20:04 -0300

    style

---
+++ src/styles/buttons.ts
@@ -0,0 +1,20 @@
+export const smRounded = {
+    borderRadius: 10
+}
+export const rounded = {
+    borderRadius: 40
+}
+export const roundy = {
+    borderRadius: 75
+}
+
+export const small = {
+    width: 24
+}
+export const medium = {
+    width: 44
+}
+export const big = {
+    width: 74
+}
+
