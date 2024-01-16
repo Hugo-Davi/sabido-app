89410883c5ec486ce5ef61c94e957ab33f826350
Hugo-Davi <111790130+Hugo-Davi@users.noreply.github.com>
2024-01-15 23:48:54 -0300

    mock

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,18 +1,26 @@
-import { useEffect } from "react";
-import { View, Text } from "react-native";
+import { useEffect, useState } from "react";
+import { View, Text, Button } from "react-native";
+import { Cards } from "@/constants/mock";
 
 export default function Home() {
-    
+    const [cards, setCards] = useState<typeof Cards>(Cards)
     useEffect(() => {
-        console.log('salve');
-        
+        async function getData() {
+            const response = Cards;
+            setCards(response);
+        }
+        getData()
    }, [])
 
     return (
         <View>
             <Text style={{fontSize: 44}}>
                 Decks
+                <Button onPress={() => console.log(cards)} title="aperta" color="red" />
             </Text>
+            {cards.map((card) => {
+                return(<Text key={card._id}>{card._id}</Text>)
+            })}
             <View>
 
             </View>
---
+++ src/constants/mock.ts
@@ -0,0 +1,47 @@
+export const Cards = [
+    {
+        "_id": "1",
+        "name": "Inglês",
+        "owner": "fulano",
+        "text": "alalalala",
+        "type": "Inglês",
+        "cards": [
+            {
+                "front": "Qual animal que come com o rabo?",
+                "back": "todos",
+                "nextReview": 1234567,
+                "learningLevel": 3
+            }
+        ]
+    },
+    {
+        "_id": "2",
+        "name": "Matemática",
+        "owner": "ciclano",
+        "text": "blah blah blah",
+        "type": "Matemática",
+        "cards": [
+            {
+                "front": "Quanto é 2 + 2?",
+                "back": "4",
+                "nextReview": 1234567,
+                "learningLevel": 2
+            }
+        ]
+    },
+    {
+        "_id": "3",
+        "name": "História",
+        "owner": "beltrano",
+        "text": "lorem ipsum",
+        "type": "História",
+        "cards": [
+            {
+                "front": "Quem foi o primeiro presidente do Brasil?",
+                "back": "Marechal Deodoro da Fonseca",
+                "nextReview": 1234567,
+                "learningLevel": 1
+            }
+        ]
+    }
+]
